1d84204064af3d66e097cd6a3a14083cec66fad6
Gadik <kkpomsk@gmail.com>
2019-02-06 09:22:01 +0000

    main example added

--- src/main/java/it/sevenbits/Main.java
+++ src/main/java/it/sevenbits/Main.java
@@ -1,7 +1,11 @@
 package it.sevenbits;
 
 import it.sevenbits.IO.*;
+import it.sevenbits.formaters.IFormatter;
+import it.sevenbits.formaters.sm.StateMachineFormatter;
 import it.sevenbits.lexers.ILexer;
+import it.sevenbits.lexers.LexerException;
+import it.sevenbits.lexers.LexerStateMachineFactory;
 import it.sevenbits.lexers.sm.LexerStateMachine;
 
 import java.io.IOException;
@@ -24,10 +28,32 @@ public class Main {
         }
         System.out.println(r.toString());
 
-        IReader someCode = new StringReader(" sad  f\" dfgg \"{f{} f }fs \'\n\' d g{} ");
+        IReader someCode = new StringReader(" sad  f\" dfgg \";{f{} / /s;;  f }fs \'\n\' d g{} ");
         ILexer lex = new LexerStateMachine(someCode);
 
         IReader someCode1 = new StringReader(" {//sdfgdfh2354\n} /*sd\n*\n***/");
         ILexer lex1 = new LexerStateMachine(someCode1);
+
+        IFormatter formatter = new StateMachineFormatter(new LexerStateMachineFactory());
+        r = new StringWritter();
+        someCode = new StringReader(" sad  f\" dfgg \";{f{} //s{}}};;  f }fs \'\n\' d g{}}");
+
+        try {
+            formatter.format(someCode, r);
+        } catch (LexerException e1) {
+            e1.printStackTrace();
+        }
+
+        System.out.println(r.toString());
+        r = new StringWritter();
+        someCode1 = new StringReader("{{{{}}}}");
+
+        try {
+            formatter.format(someCode1, r);
+        } catch (LexerException e1) {
+            e1.printStackTrace();
+        }
+
+        System.out.println(r.toString());
     }
 }
