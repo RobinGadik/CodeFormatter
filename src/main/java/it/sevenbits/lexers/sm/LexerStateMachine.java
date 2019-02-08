0caa9a60471cdcdd860fe0eb9a123e9e0ec2f067
Gadik <kkpomsk@gmail.com>
2019-02-08 10:19:17 +0000

    Lexer state machine fix

--- src/main/java/it/sevenbits/lexers/sm/LexerStateMachine.java
+++ src/main/java/it/sevenbits/lexers/sm/LexerStateMachine.java
@@ -4,14 +4,11 @@ import it.sevenbits.IO.IReader;
 import it.sevenbits.lexers.ILexer;
 import it.sevenbits.lexers.LexerException;
 import it.sevenbits.lexers.lexems.CloneCombiner;
-import it.sevenbits.lexers.lexems.CommentStartCombiner;
-import it.sevenbits.lexers.lexems.EndBlockCombiner;
-import it.sevenbits.lexers.lexems.EndLineCombiner;
+import it.sevenbits.lexers.lexems.SemiCombiner;
 import it.sevenbits.lexers.lexems.ILexemCombiner;
 import it.sevenbits.lexers.lexems.IgnoreCombiner;
 import it.sevenbits.lexers.lexems.InlineCommentCombiner;
 import it.sevenbits.lexers.lexems.MultilineCommentCombiner;
-import it.sevenbits.lexers.lexems.OpenBlockCombiner;
 import it.sevenbits.lexers.lexems.SpaceCombiner;
 import it.sevenbits.lexers.lexems.StringLiteralCombiner;
 import tokens.CharTokenMap;
@@ -52,21 +49,18 @@ public class LexerStateMachine  implements ILexer {
             charsTokens.add(charsMap.getToken(in.read()));
         }
         combs.put(new Pair<>("CLONE", "CUSTOM"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("CLONE", "OPEN_BLOCK"), new OpenBlockCombiner(tokens, buff));
-        combs.put(new Pair<>("CLONE", "END_BLOCK"), new EndBlockCombiner(tokens, buff));
-        combs.put(new Pair<>("CLONE", "SPACE"), new SpaceCombiner(tokens, buff));
-        combs.put(new Pair<>("CLONE", "END_LINE"), new EndLineCombiner(tokens, buff));
+        combs.put(new Pair<>("CLONE", "END_LINE"), new SemiCombiner(tokens, buff));
         combs.put(new Pair<>("CLONE", "STAR"), new CloneCombiner(tokens, buff));
         combs.put(new Pair<>("CLONE", "SLASH"), new CloneCombiner(tokens, buff));
 
-        combs.put(new Pair<>("SLASH", "SLASH"), new CommentStartCombiner(tokens, buff));
+        combs.put(new Pair<>("SLASH", "SLASH"), new SemiCombiner(tokens, buff));
 
         combs.put(new Pair<>("SUSPECT_COMMENT", "CUSTOM"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("SUSPECT_COMMENT", "OPEN_BLOCK"), new OpenBlockCombiner(tokens, buff));
-        combs.put(new Pair<>("SUSPECT_COMMENT", "END_BLOCK"), new EndBlockCombiner(tokens, buff));
+        combs.put(new Pair<>("SUSPECT_COMMENT", "OPEN_BLOCK"), new SemiCombiner(tokens, buff));
+        combs.put(new Pair<>("SUSPECT_COMMENT", "END_BLOCK"), new SemiCombiner(tokens, buff));
         combs.put(new Pair<>("SUSPECT_COMMENT", "SPACE"), new SpaceCombiner(tokens, buff));
         combs.put(new Pair<>("SUSPECT_COMMENT", "QUOTE"), new StringLiteralCombiner(tokens, buff));
-        combs.put(new Pair<>("SUSPECT_COMMENT", "END_LINE"), new EndLineCombiner(tokens, buff));
+        combs.put(new Pair<>("SUSPECT_COMMENT", "END_LINE"), new SemiCombiner(tokens, buff));
         combs.put(new Pair<>("SUSPECT_COMMENT", "STAR"), new MultilineCommentCombiner(tokens, buff));
         combs.put(new Pair<>("SUSPECT_COMMENT", "SLASH"), new InlineCommentCombiner(tokens, buff));
 
@@ -76,7 +70,7 @@ public class LexerStateMachine  implements ILexer {
         combs.put(new Pair<>("INLINE_COMMENT", "END_BLOCK"), new InlineCommentCombiner(tokens, buff));
         combs.put(new Pair<>("INLINE_COMMENT", "SPACE"), new InlineCommentCombiner(tokens, buff));
         combs.put(new Pair<>("INLINE_COMMENT", "QUOTE"), new InlineCommentCombiner(tokens, buff));
-        combs.put(new Pair<>("INLINE_COMMENT", "END_LINE"), new EndLineCombiner(tokens, buff));
+        combs.put(new Pair<>("INLINE_COMMENT", "END_LINE"), new SemiCombiner(tokens, buff));
         combs.put(new Pair<>("INLINE_COMMENT", "STAR"), new InlineCommentCombiner(tokens, buff));
         combs.put(new Pair<>("INLINE_COMMENT", "SLASH"), new InlineCommentCombiner(tokens, buff));
 
@@ -89,14 +83,7 @@ public class LexerStateMachine  implements ILexer {
         combs.put(new Pair<>("MULTILINE_COMMENT", "STAR"), new MultilineCommentCombiner(tokens, buff));
         combs.put(new Pair<>("MULTILINE_COMMENT", "SLASH"), new MultilineCommentCombiner(tokens, buff));
 
-        combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "CUSTOM"), new MultilineCommentCombiner(tokens, buff));
-        combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "OPEN_BLOCK"), new MultilineCommentCombiner(tokens, buff));
-        combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "END_BLOCK"), new MultilineCommentCombiner(tokens, buff));
-        combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "SPACE"), new MultilineCommentCombiner(tokens, buff));
-        combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "QUOTE"), new MultilineCommentCombiner(tokens, buff));
-        combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "END_LINE"), new MultilineCommentCombiner(tokens, buff));
         combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "STAR"), new MultilineCommentCombiner(tokens, buff));
-        combs.put(new Pair<>("MULTILINE_COMMENT_SUSPECT_END", "SLASH"), new MultilineCommentCombiner(tokens, buff));
 
         combs.put(new Pair<>("MULTILINE_COMMENT_END", "CUSTOM"), new MultilineCommentCombiner(tokens, buff));
         combs.put(new Pair<>("MULTILINE_COMMENT_END", "OPEN_BLOCK"), new MultilineCommentCombiner(tokens, buff));
@@ -110,59 +97,36 @@ public class LexerStateMachine  implements ILexer {
         combs.put(new Pair<>("END_LINE", "END_LINE"), new IgnoreCombiner(tokens, buff));
 
         combs.put(new Pair<>("IGNORE", "CUSTOM"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("IGNORE", "OPEN_BLOCK"), new OpenBlockCombiner(tokens, buff));
-        combs.put(new Pair<>("IGNORE", "END_BLOCK"), new EndBlockCombiner(tokens, buff));
+        combs.put(new Pair<>("IGNORE", "OPEN_BLOCK"), new SemiCombiner(tokens, buff));
+        combs.put(new Pair<>("IGNORE", "END_BLOCK"), new SemiCombiner(tokens, buff));
         combs.put(new Pair<>("IGNORE", "SPACE"), new IgnoreCombiner(tokens, buff));
         combs.put(new Pair<>("IGNORE", "QUOTE"), new StringLiteralCombiner(tokens, buff));
-        combs.put(new Pair<>("IGNORE", "END_LINE"), new EndLineCombiner(tokens, buff));
+        combs.put(new Pair<>("IGNORE", "END_LINE"), new SemiCombiner(tokens, buff));
         combs.put(new Pair<>("IGNORE", "STAR"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("IGNORE", "SLASH"), new CommentStartCombiner(tokens, buff));
+        combs.put(new Pair<>("IGNORE", "SLASH"), new SemiCombiner(tokens, buff));
 
-        combs.put(new Pair<>("SPACE", "CUSTOM"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("SPACE", "OPEN_BLOCK"), new OpenBlockCombiner(tokens, buff));
-        combs.put(new Pair<>("SPACE", "END_BLOCK"), new EndBlockCombiner(tokens, buff));
         combs.put(new Pair<>("SPACE", "SPACE"), new SpaceCombiner(tokens, buff));
-        combs.put(new Pair<>("SPACE", "QUOTE"), new StringLiteralCombiner(tokens, buff));
-        combs.put(new Pair<>("SPACE", "END_LINE"), new EndLineCombiner(tokens, buff));
-        combs.put(new Pair<>("SPACE", "STAR"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("SPACE", "SLASH"), new CommentStartCombiner(tokens, buff));
 
         combs.put(new Pair<>("STRING_LITERAL", "CUSTOM"), new StringLiteralCombiner(tokens, buff));
         combs.put(new Pair<>("STRING_LITERAL", "OPEN_BLOCK"), new StringLiteralCombiner(tokens, buff));
         combs.put(new Pair<>("STRING_LITERAL", "END_BLOCK"), new StringLiteralCombiner(tokens, buff));
         combs.put(new Pair<>("STRING_LITERAL", "SPACE"), new StringLiteralCombiner(tokens, buff));
         combs.put(new Pair<>("STRING_LITERAL", "QUOTE"), new StringLiteralCombiner(tokens, buff));
-        combs.put(new Pair<>("STRING_LITERAL", "END_LINE"), new StringLiteralCombiner(tokens, buff));
+        combs.put(new Pair<>("STRING_LITERAL", "END_LINE"), new IgnoreCombiner(tokens, buff));
         combs.put(new Pair<>("STRING_LITERAL", "STAR"), new StringLiteralCombiner(tokens, buff));
         combs.put(new Pair<>("STRING_LITERAL", "SLASH"), new StringLiteralCombiner(tokens, buff));
 
-        combs.put(new Pair<>("END_STRING_LITERAL", "CUSTOM"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("END_STRING_LITERAL", "OPEN_BLOCK"), new OpenBlockCombiner(tokens, buff));
-        combs.put(new Pair<>("END_STRING_LITERAL", "END_BLOCK"), new EndBlockCombiner(tokens, buff));
-        combs.put(new Pair<>("END_STRING_LITERAL", "SPACE"), new SpaceCombiner(tokens, buff));
         combs.put(new Pair<>("END_STRING_LITERAL", "QUOTE"), new StringLiteralCombiner(tokens, buff));
-        combs.put(new Pair<>("END_STRING_LITERAL", "END_LINE"), new EndLineCombiner(tokens, buff));
-        combs.put(new Pair<>("END_STRING_LITERAL", "STAR"), new CloneCombiner(tokens, buff));
-        combs.put(new Pair<>("END_STRING_LITERAL", "SLASH"), new CommentStartCombiner(tokens, buff));
 
-        combs.put(new Pair<>("OPEN_BLOCK", "OPEN_BLOCK"), new OpenBlockCombiner(tokens, buff));
+        combs.put(new Pair<>("OPEN_BLOCK", "OPEN_BLOCK"), new SemiCombiner(tokens, buff));
 
-        combs.put(new Pair<>("END_BLOCK", "END_BLOCK"), new EndBlockCombiner(tokens, buff));
+        combs.put(new Pair<>("END_BLOCK", "END_BLOCK"), new SemiCombiner(tokens, buff));
 
         for (IToken charToken : charsTokens) {
             buff.add(charToken);
             currentState = stateMap.getNextState(currentState, charToken.getType());
-            //System.out.println(currentState);
-            //System.out.println(buff);
             combs.get(new Pair<>(currentState.toString(), charToken.getType())).execute();
-            //System.out.println("FIUUUUU uuu uu u u u u u u ");
-            //System.out.println(tokens);
         }
-
-        /*System.out.println(tokens.size());
-        for (IToken t : tokens) {
-            //System.out.println(t.toString());
-        }*/
 
 
     }
--- src/main/java/it/sevenbits/lexers/sm/StateMap.java
+++ src/main/java/it/sevenbits/lexers/sm/StateMap.java
@@ -38,7 +38,6 @@ class StateMap {
         states.put(new Pair<>(defaultState, "SPACE"), spaceUno);
         states.put(new Pair<>(defaultState, "QUOTE"), stringLiteral);
         states.put(new Pair<>(defaultState, "SLASH"), slash);
-        states.put(new Pair<>(defaultState, "END_LINE"), endLine);
 
         states.put(new Pair<>(stringLiteral, "OPEN_BLOCK"), stringLiteral);
         states.put(new Pair<>(stringLiteral, "END_BLOCK"), stringLiteral);
@@ -53,13 +52,11 @@ class StateMap {
         states.put(new Pair<>(endStringLiteral, "END_BLOCK"), endBlock);
         states.put(new Pair<>(endStringLiteral, "SPACE"), spaceUno);
         states.put(new Pair<>(endStringLiteral, "SLASH"), slash);
-        states.put(new Pair<>(endStringLiteral, "END_LINE"), endLine);
 
         states.put(new Pair<>(slash, "QUOTE"), stringLiteral);
         states.put(new Pair<>(slash, "OPEN_BLOCK"), openBlock);
         states.put(new Pair<>(slash, "END_BLOCK"), endBlock);
         states.put(new Pair<>(slash, "SPACE"), spaceUno);
-        states.put(new Pair<>(slash, "END_LINE"), endLine);
         states.put(new Pair<>(slash, "SLASH"), inlineComment);
         states.put(new Pair<>(slash, "STAR"), multilineComment);
 
@@ -67,7 +64,6 @@ class StateMap {
         states.put(new Pair<>(commentSuspection, "OPEN_BLOCK"), openBlock);
         states.put(new Pair<>(commentSuspection, "END_BLOCK"), endBlock);
         states.put(new Pair<>(commentSuspection, "SPACE"), spaceUno);
-        states.put(new Pair<>(commentSuspection, "END_LINE"), endLine);
         states.put(new Pair<>(commentSuspection, "SLASH"), inlineComment);
         states.put(new Pair<>(commentSuspection, "STAR"), multilineComment);
 
@@ -78,7 +74,6 @@ class StateMap {
         states.put(new Pair<>(inlineComment, "CUSTOM"), inlineComment);
         states.put(new Pair<>(inlineComment, "SLASH"), inlineComment);
         states.put(new Pair<>(inlineComment, "STAR"), inlineComment);
-        states.put(new Pair<>(inlineComment, "END_LINE"), endLine);
 
         states.put(new Pair<>(multilineComment, "QUOTE"), multilineComment);
         states.put(new Pair<>(multilineComment, "OPEN_BLOCK"), multilineComment);
@@ -98,33 +93,35 @@ class StateMap {
         states.put(new Pair<>(multilineCommentSuspectEnd, "END_LINE"), multilineComment);
         states.put(new Pair<>(multilineCommentSuspectEnd, "STAR"), multilineCommentSuspectEnd);
 
+        states.put(new Pair<>(multilineCommentEnd, "QUOTE"), stringLiteral);
+        states.put(new Pair<>(multilineCommentEnd, "OPEN_BLOCK"), openBlock);
+        states.put(new Pair<>(multilineCommentEnd, "END_BLOCK"), endBlock);
+        states.put(new Pair<>(multilineCommentEnd, "SPACE"), spaceUno);
+        states.put(new Pair<>(multilineCommentEnd, "SLASH"), slash);
+
         states.put(new Pair<>(spaceUno, "SPACE"), ignore);
         states.put(new Pair<>(spaceUno, "OPEN_BLOCK"), openBlock);
         states.put(new Pair<>(spaceUno, "END_BLOCK"), endBlock);
         states.put(new Pair<>(spaceUno, "QUOTE"), stringLiteral);
         states.put(new Pair<>(spaceUno, "SLASH"), slash);
-        states.put(new Pair<>(spaceUno, "END_LINE"), endLine);
 
         states.put(new Pair<>(ignore, "OPEN_BLOCK"), openBlock);
         states.put(new Pair<>(ignore, "END_BLOCK"), endBlock);
         states.put(new Pair<>(ignore, "SPACE"), ignore);
         states.put(new Pair<>(ignore, "QUOTE"), stringLiteral);
         states.put(new Pair<>(ignore, "SLASH"), slash);
-        states.put(new Pair<>(ignore, "END_LINE"), endLine);
 
         states.put(new Pair<>(openBlock, "END_BLOCK"), endBlock);
         states.put(new Pair<>(openBlock, "OPEN_BLOCK"), openBlock);
         states.put(new Pair<>(openBlock, "SPACE"), ignore);
         states.put(new Pair<>(openBlock, "QUOTE"), stringLiteral);
         states.put(new Pair<>(openBlock, "SLASH"), slash);
-        states.put(new Pair<>(openBlock, "END_LINE"), endLine);
 
         states.put(new Pair<>(endBlock, "OPEN_BLOCK"), openBlock);
         states.put(new Pair<>(endBlock, "END_BLOCK"), endBlock);
         states.put(new Pair<>(endBlock, "SPACE"), ignore);
         states.put(new Pair<>(endBlock, "QUOTE"), stringLiteral);
         states.put(new Pair<>(endBlock, "SLASH"), slash);
-        states.put(new Pair<>(endBlock, "END_LINE"), endLine);
     }
 
     /**
