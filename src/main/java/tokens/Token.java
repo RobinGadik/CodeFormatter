7ed8e256c12cdb218d6d29c4ae7c5335dcb130e6
Gadik <kkpomsk@gmail.com>
2019-02-08 10:11:12 +0000

    Token equals and hashCode methods insert

--- src/main/java/tokens/Token.java
+++ src/main/java/tokens/Token.java
@@ -7,6 +7,30 @@ public class Token implements IToken {
     private String text;
     private String type;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Token token = (Token) o;
+
+        if (getText() != null ? !getText().equals(token.getText()) : token.getText() != null) {
+            return false;
+        }
+        return getType() != null ? getType().equals(token.getType()) : token.getType() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getText() != null ? getText().hashCode() : 0;
+        result = 31 * result + (getType() != null ? getType().hashCode() : 0);
+        return result;
+    }
+
     /**
      * @param text token text
      * @param type token type
